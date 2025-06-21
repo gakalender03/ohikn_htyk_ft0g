16c4e89ebe441ce242541a97aab75324ec8f4153
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-21 11:04:22 +0700

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -5,8 +5,9 @@ const { ethers } = require('ethers');
   try {
     const privateKey = '0x81f8cb133e86d1ab49dd619581f2d37617235f59f1398daee26627fdeb427fbe'; // replace with your test PK
     const wallet = new ethers.Wallet(privateKey);
-    const address = await wallet.getAddress(); // Wait for the address to resolve
-    const recipient = address.toLowerCase();   // Now apply .toLowerCase()
+    const address = await wallet.getAddress();
+    console.log('Address:', address, typeof address); // Debug: Check the type of address
+    const recipient = address.toLowerCase();
 
     const txHash = await sendTestETH({
       privateKey,
