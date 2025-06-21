887eb458da3bb21c6dfe64770e572f8fbc4f8973
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-21 07:45:13 +0700

    Update operan3.js

--- operan3.js
+++ operan3.js
@@ -2,7 +2,7 @@
 const { ethers } = require('ethers');
 
 // Replace with the actual Sei EVM RPC URL (for testing purposes)
-const seiEvmRpcUrl = 'https://evm-rpc-testnet.sei-apis.com'; // Example: 'https://sei-evm-rpc-url.com'
+const seiEvmRpcUrl = 'https://rpc.ankr.com/corn_testnet'; // Example: 'https://sei-evm-rpc-url.com'
 
 // Create a provider to connect to Sei EVM
 const provider = new ethers.JsonRpcProvider(seiEvmRpcUrl);
