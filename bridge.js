1973613b19b1452e254abfa348e51cf2ce263940
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-21 08:19:29 +0700

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,13 +1,14 @@
 const { sendTestETH } = require('./seitocorn.js');
 
-const main = async () => {
-  const txHash = await sendTestETH({
-    sourceChain: 'SEI',
-    destChain: 'CORN',
-    privateKey: '0x81f8cb133e86d1ab49dd619581f2d37617235f59f1398daee26627fdeb427fbe
-  });
-
-  console.log('Tx hash:', txHash);
-};
-
-main().catch(console.error);
+(async () => {
+  try {
+    const txHash = await sendTestETH({
+      sourceChain: 'SEI',
+      destChain: 'CORN',
+      privateKey: '0x81f8cb133e86d1ab49dd619581f2d37617235f59f1398daee26627fdeb427fbe'
+    });
+    console.log('Tx hash:', txHash);
+  } catch (err) {
+    console.error('Error sending test ETH:', err.message);
+  }
+})();
