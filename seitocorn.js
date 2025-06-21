b5ca119856aafb9580074b77cedecda0706d7afb
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-21 08:29:40 +0700

    Update seitocorn.js

--- seitocorn.js
+++ seitocorn.js
@@ -1,5 +1,5 @@
 const { ethers } = require('ethers');
-import {
+const {
   CHAINS,
   RPC_URLS,
   UNION_CONTRACT,
@@ -7,7 +7,7 @@ import {
   GAS_SETTINGS,
   RPC_TIMEOUTS,
   TRANSACTION_SETTINGS
-} from './config.js';
+} require=('./config.js');
 
 const providerCache = new Map();
 
