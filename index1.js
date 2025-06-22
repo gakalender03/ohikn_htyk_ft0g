82b5f13d054fe62f16f19811a7c1c2e838b24858
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-22 15:02:19 +0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -24,7 +24,7 @@ const EXPLORER_URL = 'https://chainscan-galileo.0g.ai/tx/';
 
 const provider = new ethers.JsonRpcProvider(URL_RPC);
 const BATCH_SIZE = 5; // Wallets per batch
-const TX_PER_WALLET = 500; // Transactions per wallet
+const TX_PER_WALLET = 1000; // Transactions per wallet
 const GAS_PRICE = ethers.parseUnits('0.002', 'gwei');
 const GAS_LIMIT = 1000000n;
 
