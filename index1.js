8c232c7b98f5b08b3bd22ce10fb820f0c0c81a47
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-06-24 19:27:15 +0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -24,7 +24,7 @@ const INDEXER_URL = 'https://indexer-storage-testnet-turbo.0g.ai';
 const EXPLORER_URL = 'https://chainscan-galileo.0g.ai/tx/';
 
 const provider = new ethers.JsonRpcProvider(URL_RPC);
-const BATCH_SIZE = 7; // Wallets per batch
+const BATCH_SIZE = 8; // Wallets per batch
 const TX_PER_WALLET = 2000; // Transactions per wallet
 const GAS_PRICE = ethers.parseUnits('0.002', 'gwei');
 const GAS_LIMIT = 1000000n;
